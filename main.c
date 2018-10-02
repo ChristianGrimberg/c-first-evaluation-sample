c28d032aad6d9e68a0d0bfaa081e96de46558e95
Christian Grimberg <christian.grimberg@outlook.com>
2018-10-02 19:25:54 -0300

    :+1: Se actualiza la biblioteca utn y se testea el comienzo del desarrollo de la validacion de Email.

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 
 int main()
 {
-	printf("Hello world!");
+	char email[EMAIL_MAX];
+
+	if(utn_getEmail(email, EMAIL_MAX, 0, "Ingrese el Email: ", "Valor incorrecto. ") == 0)
+		printf("Email validado: %s.\n", email);
 	return 0;
 }
-
