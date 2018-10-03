78afbab1c50b329a40c7eee5a9ce7e5e866865a0
Christian Grimberg <christian.grimberg@outlook.com>
2018-10-02 23:42:47 -0300

    :bug: Se corrige un error de validacion del CUIT.

--- main.c
+++ main.c
@@ -3,11 +3,15 @@
 int main()
 {
 	char email[EMAIL_MAX];
+	char cuit[24];
 
 	system("clear");
 	if(utn_getEmail(email, EMAIL_MAX, 10, "Ingrese el Email: ", "Valor incorrecto. ") == 0)
 		printf("Email validado: %s\n", email);
 	else
 		printf("No se ingreso un correo.\n");
+
+	if(utn_getCUIT(cuit, 24, 3, "Ingrese el CUIT: ", "Valor incorrecto. ") == 0)
+		printf("CUIT validado: %s\n", cuit);
 	return 0;
 }
